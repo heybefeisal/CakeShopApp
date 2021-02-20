446914cc9bcdfb8209c6f67ed42a044f3c5548db
Heybe Feisal <46941322+heybefeisal@users.noreply.github.com>
2021-02-20 18:25:55 +0100

    flytt till pc

---
+++ CakeShopApp/Controllers/PieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CakeShopApp.Models;
+using CakeShopApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CakeShopApp.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            ViewBag.CurrentCategory = "Cheese cakes";
+            return View(pieListViewModel);
+        }
+    }
+}
---
+++ CakeShopApp/ViewModels/PieListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CakeShopApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeShopApp.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; } 
+    }
+}
